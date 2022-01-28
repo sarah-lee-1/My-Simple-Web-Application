cfc76f1fdec8637a04870133ca7eeb8f43e74098
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-01-27 21:18:10 -0600

    Index.js code corrections

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi there nice person');
+    res.send('Hi there nice person! ');
 });
 
 app.listen(8081, () => {
